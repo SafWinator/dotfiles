bf75b67c10b2a150381858268690106ad3c55ace
SafWinator <Safwaan245654346@gmail.com>
2022-02-04 19:03:15 +0000

    another commit

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar makc 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Polybar launched..."
+# Launch bar
+polybar makc -r -c ~/.config/polybar/config &
